1fa4f41712ef67242b9be4c5978f2e1c3acbee08
daliahaemmerle <dalia1996@gmx.at>
2018-03-27 16:59:22 +0200

    GridView Adapter

--- app/src/main/java/com/example/katharinafeiertag/mediary/EinnahmeActivity.java
+++ app/src/main/java/com/example/katharinafeiertag/mediary/EinnahmeActivity.java
@@ -3,6 +3,11 @@ package com.example.katharinafeiertag.mediary;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.GridView;
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.TextView;
 
 public class EinnahmeActivity extends AppCompatActivity {
 
@@ -10,7 +15,25 @@ public class EinnahmeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_einnahme);
+
+        final String[] tage = getResources().getStringArray(R.array.wochentage);
+        GridView gridview = (GridView) findViewById(R.id.gv);
+
+        ArrayAdapter<String> countryAdapter = new
+                ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, tage);
+        gridview.setAdapter(countryAdapter);
+
+        //Listener
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view,
+                                    int position, long id) {
+                String s = ((TextView) view).getText() + " " + position;
+                Toast.makeText(EinnahmeActivity.this, tage[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 
     public void onClickClose(View view) {
         finish();
