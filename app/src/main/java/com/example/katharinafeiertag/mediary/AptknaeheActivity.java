2eb53b41e97e1a795a03dd74c7fb5e3a2390c97e
Feiert Kati <katharina.feiertag@gmail.com>
2018-04-17 15:00:50 +0200

    Google Maps -> Tutorial Part 2 6:20 erledigt

--- app/src/main/java/com/example/katharinafeiertag/mediary/AptknaeheActivity.java
+++ app/src/main/java/com/example/katharinafeiertag/mediary/AptknaeheActivity.java
@@ -1,18 +1,42 @@
 package com.example.katharinafeiertag.mediary;
 
+import android.app.Dialog;
+import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class AptknaeheActivity extends AppCompatActivity {
+
+    private static final String TAG = "AptknaeheAcitvity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aptknaehe);
+
     }
+
 
     public void onClickClose(View view) {
         finish();
     }
+
+
 }
+
--- app/src/main/java/com/example/katharinafeiertag/mediary/HauptmenuActivity.java
+++ app/src/main/java/com/example/katharinafeiertag/mediary/HauptmenuActivity.java
@@ -1,20 +1,45 @@
 package com.example.katharinafeiertag.mediary;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class HauptmenuActivity extends AppCompatActivity {
     //Aufgabenstellung: Ein Ergebnis aus einer Aktivität zurückgeben
     public static final String REQUEST_RESULT = "REQUEST_RESULT";
 
+    private static final String TAG = "AptknaeheActivity";
+
+    private static final int ERROR_DIADLOG_REQUEST = 9011;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hauptmenu);
+        if(isServiceOK()){
+            init();
+        }
+    }
+
+    private void init() {
+        ImageButton bt_apt = (ImageButton) findViewById(R.id.bt_apt);
+        bt_apt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HauptmenuActivity.this, AptknaeheActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
 
@@ -68,6 +93,27 @@ public class HauptmenuActivity extends AppCompatActivity {
                     Integer.toString(data.getIntExtra(REQUEST_RESULT, 0)),
                     Toast.LENGTH_LONG).show();
         }
+    }
+
+    public boolean isServiceOK() {
+        Log.d(TAG, "isServiceOK: checking google services version");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(AptknaeheActivity.this);
+
+        if (available  == ConnectionResult.SUCCESS) {
+            //erverything is fine and the user can make requests
+            Log.d(TAG, "isServiceOK: Google Play Services is working");
+            return true;
+
+        } else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)) {
+            //an error occured but we can resolve it
+            Log.d(TAG, "isServiceOK: an error occured but we can fix it");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(AptknaeheActivity.this,available, ERROR_DIADLOG_REQUEST);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "We can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 
 }
