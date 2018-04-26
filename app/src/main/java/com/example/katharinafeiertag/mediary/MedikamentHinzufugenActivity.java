8141c30a52138c07a2503666eaac8191a9161752
daliahaemmerle <dalia1996@gmx.at>
2018-04-26 13:55:34 +0200

    Medikamenten excel Datenbankanbindung + Service (Background) + Google Kathi

--- app/src/main/java/com/example/katharinafeiertag/mediary/MedikamentHinzufugenActivity.java
+++ app/src/main/java/com/example/katharinafeiertag/mediary/MedikamentHinzufugenActivity.java
@@ -1,13 +1,118 @@
 package com.example.katharinafeiertag.mediary;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.AppCompatActivity;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.Button;
+
+
+import java.io.InputStream;
+
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
 
 public class MedikamentHinzufugenActivity extends AppCompatActivity {
+
+    String value;
+    private TextView tv;
+    private ExampleBroadcastReciever resultReciever;
+    ServiceDbAbfrage servicedbabfrage;
+    Button start;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_medikamenthinzufugen);
+        tv = (TextView) findViewById(R.id.tv_anzeige);
+        registerExampleBroadcastReceiver();
+        start = (Button) findViewById(R.id.bt_show);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        // When the function stops, the receiver needs to be unregistered
+        unregisterReceiver(resultReciever);
+    }
+
+    public void anzeigen(View v) {
+
+        int wartezeit;
+
+    try{
+        /*Toast toasti = Toast.makeText(MedikamentHinzufugenActivity.this, "Datenbank wird durchgesucht", Toast.LENGTH_SHORT);
+        toasti.show();*/
+
+        wartezeit = 5000;
+        AssetManager am = getAssets();
+        InputStream is = am.open("versuch.xls");
+        Workbook wb = Workbook.getWorkbook(is);
+        Sheet s = wb.getSheet(0);
+        int row = s.getRows();
+        int col = s.getColumns();
+        String xx="";
+
+        for(int i=0;i<row;i++)
+        {
+            for(int c=0;c<col;c++)
+            {
+                Cell z = s.getCell(c,i);
+                xx =xx+z.getContents();
+            }
+            xx=xx+"\n";
+        }
+        display(xx);
+    }
+    catch(Exception e){
+        wartezeit = 0;
+        Toast toasti2 = Toast.makeText(MedikamentHinzufugenActivity.this,"Fehlgeschlagen",Toast.LENGTH_SHORT);
+        toasti2.show();
+    }
+
+        Intent intent = new Intent(getBaseContext(),ServiceDbAbfrage.class);
+        intent.putExtra("wartezeit",wartezeit);
+        startService(intent);
+
+    }
+
+    public void display(String value) {
+        TextView example = (TextView) findViewById(R.id.tv_versuch);
+        example.setMovementMethod(new ScrollingMovementMethod());
+        example.setText(value);
+    }
+
+    private void registerExampleBroadcastReceiver(){
+        // Create a broadcast receiver
+        resultReciever = new ExampleBroadcastReciever();
+
+        // Create an intent that indicates which broadcasts to listen for
+        IntentFilter intentFilter = new IntentFilter();
+        //intentFilter.addAction(ExampleIntentService.EXAMPLE_ACTION);
+        intentFilter.addAction(ServiceDbAbfrage.EXAMPLE_ACTION);
+
+        // Register the receiver
+        registerReceiver(resultReciever, intentFilter);
+    }
+
+
+    private class ExampleBroadcastReciever extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String ex_info = intent.getStringExtra(ServiceDbAbfrage.EXAMPLE_CATEGORY);
+            tv.setText(ex_info);
+        }
     }
 }
---
+++ app/src/main/java/com/example/katharinafeiertag/mediary/ServiceDbAbfrage.java
@@ -0,0 +1,84 @@
+package com.example.katharinafeiertag.mediary;
+
+import android.app.NotificationManager;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by scheerbernhard on 26.04.18.
+ */
+
+public class ServiceDbAbfrage extends Service {
+    final static String EXAMPLE_ACTION = "example_action";          // Example action for client communication
+    final static String EXAMPLE_CATEGORY = "example_cat";           // Example category to filter extra infor form client
+    final static String EXAMPLE_NAME = "Example ScheduledJob";     // Name of the service
+    final static int DEFAULT_PROCESSING = 3000;                     // Default processing time
+    private NotificationManager mNM;
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+
+    public ServiceDbAbfrage() {
+
+    }
+
+    /**
+     * Diese Methode wird gestartet, wenn der Benutzer ein neues Medikament
+     * zu seiner digitalen Hausapotheke hinzufügt
+     * @param intent
+     * @param flags
+     * @param startId
+     * @return
+     */
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        int wartezeit = intent.getExtras().getInt("wartezeit");
+
+        try {
+            Thread.sleep(wartezeit);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        String msg = "Ihre digitale Hausapotheke wurde aktualisiert";
+
+        broadcastExampleInfo(msg);
+        stopSelf();
+
+        return Service.START_STICKY;
+    }
+
+
+    @Override
+    public IBinder onBind (Intent intent) {
+        return null;
+    }
+
+    private void broadcastExampleInfo(String msg){
+        Intent intent = new Intent();
+        intent.setAction(EXAMPLE_ACTION);
+        intent.putExtra(EXAMPLE_CATEGORY, msg);
+        sendBroadcast(intent);
+    }
+
+    /**
+     *
+     * @return
+     */
+    public boolean onStopService() {
+        Toast toasti = Toast.makeText(ServiceDbAbfrage.this, "Die Aktualisierung Ihrer Datenbank ist fehlgeschlagen.", Toast.LENGTH_SHORT);
+        toasti.show();
+        Log.d("stop","Vorgang gestoppt.");
+
+        return false;
+    }
+
+
+}
